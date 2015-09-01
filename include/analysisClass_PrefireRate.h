566bcb566d701e826bc68e204d105dd2c7c6c790
lucien1011 <lucien1011@gmail.com>
2015-09-01 17:21:40 +0200

    separate prefire rate with jet pT

--- include/analysisClass_PrefireRate.h
+++ include/analysisClass_PrefireRate.h
@@ -1,25 +1,25 @@
-#ifndef analysisClass_HFPrefiring_h
-#define analysisClass_HFPrefiring_h
+#ifndef analysisClass_PrefireRate_h
+#define analysisClass_PrefireRate_h
 
 #include "analysisClass.h"
 
-class analysisClass_HFPrefiring : public analysisClass {
+class analysisClass_PrefireRate : public analysisClass {
  
  public:
-  analysisClass_HFPrefiring (std::string inputList, std::string treeList, std::string out_file): 
+  analysisClass_PrefireRate (std::string inputList, std::string treeList, std::string out_file): 
   analysisClass(inputList, treeList, out_file) {};
 
-  ~analysisClass_HFPrefiring(){};
+  ~analysisClass_PrefireRate(){};
   void loop();
   bool checkTriggerBit(const int & ibit,const int & ibx);
   void loadTrigMap();
   bool isSelectedBx(int bunchNumber);
-  double getJetPt(const std::vector<double> & jetCollPt, const std::vector<int> & jetCollBx ,const int & bx);
 
 private :
 
   std::map<std::string,int> HFPrefiringBitMap;
   std::map<std::string,int>::iterator trigbit_iter;
+  std::map<std::string,TH1F*> histoMap;
   std::vector<ULong64_t> tw1;
   std::vector<ULong64_t> tw2;
 
